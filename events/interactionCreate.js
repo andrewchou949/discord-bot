9fdcc5a1923e7f4a149483ae479af47c08a75990
Andrew <andrewchou949@gmail.com>
2024-04-24 16:14:38 -0700

    Completed the cooldown feature for each slash command

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,8 +1,9 @@
-const { Events } = require('discord.js');
+const { Events, Collection } = require('discord.js');
 
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
+        // Command segment!
         if (!interaction.isChatInputCommand()) return;
 
         const command = interaction.client.commands.get(interaction.commandName);
@@ -11,7 +12,31 @@ module.exports = {
             console.error(`No command matching ${interaction.commandName} was found.`);
             return;
         }
+        // Cooldown Segments
+        const { cooldowns } = interaction.client;
 
+        if (!cooldowns.has(command.data.name)) {
+            cooldowns.set(command.data.name, new Collection());
+        }
+
+        const now = Date.now();
+        const timestamps = cooldowns.get(command.data.name);
+        const defaultCooldownDuration = 3;
+        const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+        if (timestamps.has(interaction.user.id)) {
+            const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+            if (now < expirationTime) {
+                const expiredTimestamp = Math.round(expirationTime / 1000);
+                return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+            }
+        }
+
+        timestamps.set(interaction.user.id, now);
+        setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+        // Execution segments for both cooldown and commands!
         try {
             await command.execute(interaction);
         } catch (error) {
