564231df458045a2ba497646502a2a41eeedbca4
Andrew <andrewchou949@gmail.com>
2024-04-26 17:19:39 -0700

    Updated the reload.js

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -1,53 +1,36 @@
 const { SlashCommandBuilder } = require('discord.js');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('reload')
 		.setDescription('Reloads a command.')
-        // require another option!
 		.addStringOption(option =>
 			option.setName('command')
 				.setDescription('The command to reload.')
 				.setRequired(true)),
 	async execute(interaction) {
-        const commandName = interaction.options.getString('command', true).toLowerCase();
-        const commandsPath = path.join(__dirname, '..', 'commands');  // Adjust to the directory where commands are stored
-        const commandFiles = fs.readdirSync(commandsPath);
-        const commandFile = commandFiles.find(file => file.split('.')[0] === commandName);
+		const commandName = interaction.options.getString('command', true).toLowerCase();
 
-        if (!commandFile) {
-            return interaction.reply(`There is no command file with name \`${commandName}\`. Make sure the command file exists.`);
-        }
+		const commandsPath = path.join(__dirname); // __dirname points to the current directory of this script
 
-        const filePath = path.join(commandsPath, commandFile);
+		try {
+			const commandFile = `${commandName}.js`;
+			const filePath = path.join(commandsPath, commandFile);
 
-        try {
-            delete require.cache[require.resolve(filePath)];
-            const newCommand = require(filePath);
-            interaction.client.commands.delete(commandName);
-            interaction.client.commands.set(newCommand.data.name, newCommand);
-            await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply(`There was an error while reloading a command \`${commandName}\`:\n\`${error.message}\``);
-        }
+			if (!fs.existsSync(filePath)) {
+				return interaction.reply(`There is no command file with name \`${commandName}\` in the utility directory. Make sure the command file exists.`);
+			}
 
-		// const commandName = interaction.options.getString('command', true).toLowerCase();
-        // const command = interaction.client.commands.get(commandName);
-
-        // if (!command) {
-        //     return interaction.reply(`There is no command with name \`${commandName}\`!`);
-        // }
-        // delete require.cache[require.resolve(`./${command.data.name}.js`)];
-
-		// try {
-	    //     interaction.client.commands.delete(command.data.name);
-	    //     const newCommand = require(`./${command.data.name}.js`);
-	    //     interaction.client.commands.set(newCommand.data.name, newCommand);
-	    //     await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
-		// } catch (error) {
-	    //     console.error(error);
-	    //     await interaction.reply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
-		// }
+			delete require.cache[require.resolve(filePath)];
+			const newCommand = require(filePath);
+			interaction.client.commands.delete(commandName);
+			interaction.client.commands.set(newCommand.data.name, newCommand);
+			await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+		} catch (error) {
+			console.error('Reload command error:', error);
+			await interaction.reply(`There was an error while reloading a command \`${commandName}\`:\n\`${error.message}\``);
+		}
 	},
 };
